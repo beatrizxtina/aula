06f6120d679156b83e0b0fe83ee951744be6ee12
beatrizxtina <beatrizmedcosta@gmail.com>
2021-03-18 20:05:42 +0000

    aula

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from app import app
-
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    user = {'username': 'Bea'}
+    posts = [
+        {'author': {'username': 'Beatriz'}, 'body': "Olá da Bea"},
+        {'author': {'username': 'Fernando'}, 'body': "Olá!"}
+    ]
+    return render_template("index.html", user=user, posts=posts)
